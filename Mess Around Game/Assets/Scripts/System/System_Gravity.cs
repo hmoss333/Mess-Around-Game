abd09bbc52e0f5a98b5f24050eba7c8748aa14fb
hmoss333 <hdamoss@gmail.com>
2017-10-01 00:04:04 -0400

    Rotation fixes

--- Mess Around Game/Assets/Scripts/System/System_Gravity.cs
+++ Mess Around Game/Assets/Scripts/System/System_Gravity.cs
@@ -7,24 +7,32 @@ public class System_Gravity : MonoBehaviour {
     float force = 9.81f; //apply gravity
     public Vector2 newDir;
 
-    //private Quaternion localRotation; // 
-    //public float speed = 1.0f; // ajustable speed from Inspector in Unity editor
+    private Quaternion localRotation; // 
+    public float smooth = 1.0f; // ajustable speed from Inspector in Unity editor
+	private Quaternion smoothRot;
 
     // Use this for initialization
     void Start () {
-        //localRotation = transform.rotation;
+        localRotation = transform.rotation;
     }
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+		//smoothRot = Quaternion.Slerp (localRotation, Quaternion.AngleAxis (Input.acceleration.x * Mathf.Rad2Deg, Vector3.forward), smooth * Time.deltaTime);
 #if UNITY_EDITOR
-        newDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		smoothRot = Quaternion.Slerp (localRotation, Quaternion.AngleAxis (Input.GetAxisRaw("Horizontal") * Mathf.Rad2Deg, Vector3.forward), smooth * Time.deltaTime);
+		newDir = new Vector2(Input.GetAxisRaw("Horizontal"), -1f);
 #elif UNITY_ANDROID
-        newDir = Quaternion.AngleAxis(Input.acceleration.x * Mathf.Rad2Deg, Vector3.forward) * -Vector2.up;
+		smoothRot = Quaternion.Slerp (localRotation, Quaternion.AngleAxis (Input.acceleration.x * Mathf.Rad2Deg, Vector3.forward), smooth * Time.deltaTime);
+		newDir = smoothRot * -Vector2.up;
 #endif
 
-        Physics2D.gravity = newDir * force;
+		Physics2D.gravity = newDir * force;
+		transform.rotation = smoothRot;
+		//transform.Rotate (new Vector3(0, 0, newDir.x));
+
+		localRotation = transform.rotation;
     }
 
     void Update()
