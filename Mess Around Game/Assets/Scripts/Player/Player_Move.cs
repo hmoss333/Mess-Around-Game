365d278ba0b8f234a039ab88baed9c91ff07dd68
hmoss333 <hdamoss@gmail.com>
2017-10-11 13:47:12 -0400

    testing
    stuff

--- Mess Around Game/Assets/Scripts/Player/Player_Move.cs
+++ Mess Around Game/Assets/Scripts/Player/Player_Move.cs
@@ -6,13 +6,15 @@ public class Player_Move : MonoBehaviour {
 
     public float speed;
 
-    private float leftConstraint = 0.0f;
-    private float rightConstraint = Screen.width; //960.0f;
-    private float bottomConstraint = 0.0f;
-    private float topConstraint = Screen.height; //960.0f;
-    private float buffer = 1.0f; // set this so the spaceship disappears offscreen before re-appearing on other side
+    private float leftConstraint; // = 0.0f;
+    private float rightConstraint; // = Screen.width; //960.0f;
+    private float bottomConstraint; // = 0.0f;
+    private float topConstraint; // = Screen.height; //960.0f;
+    private float buffer = 5.0f; // set this so the ball disappears offscreen before re-appearing on other side
     Camera cam;
     private float distanceZ;
+
+    System_Gravity sgrav;
 
     // Use this for initialization
     void Start () {
@@ -23,30 +25,30 @@ public class Player_Move : MonoBehaviour {
         rightConstraint = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, distanceZ)).x;
         bottomConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, distanceZ)).y;
         topConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, Screen.height, distanceZ)).y;
+
+        sgrav = GameObject.FindObjectOfType<System_Gravity>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //Looping magic
-        //Left/Right
-        if (transform.position.x < leftConstraint - buffer)
-        {
-            transform.position = new Vector3(rightConstraint + buffer, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x > rightConstraint + buffer)
-        {
-            transform.position = new Vector3(leftConstraint - buffer, transform.position.y, transform.position.z);
-        }
-        //Top/Bottom
-        if (transform.position.y < bottomConstraint - buffer)
-        {
-            transform.position = new Vector3(transform.position.x, topConstraint + buffer, transform.position.z);
-        }
-        if (transform.position.y > topConstraint + buffer)
-        {
-            transform.position = new Vector3(transform.position.x, bottomConstraint - buffer, transform.position.z);
-        }
-
-
+        ////Looping magic
+        ////Left/Right
+        //if (transform.position.x < (leftConstraint * sgrav.newDir.x) - buffer)
+        //{
+        //    transform.position = new Vector3((rightConstraint * sgrav.newDir.x) + buffer, transform.position.y, transform.position.z);
+        //}
+        //if (transform.position.x > (rightConstraint * sgrav.newDir.x) + buffer)
+        //{
+        //    transform.position = new Vector3((leftConstraint * sgrav.newDir.x) - buffer, transform.position.y, transform.position.z);
+        //}
+        ////Top/Bottom
+        //if (transform.position.y < (bottomConstraint * sgrav.newDir.y) - buffer)
+        //{
+        //    transform.position = new Vector3(transform.position.x, (topConstraint * sgrav.newDir.y) + buffer, transform.position.z);
+        //}
+        //if (transform.position.y > (topConstraint * sgrav.newDir.y) + buffer)
+        //{
+        //    transform.position = new Vector3(transform.position.x, (bottomConstraint * sgrav.newDir.y) - buffer, transform.position.z);
+        //}
     }
 }
--- Mess Around Game/Assets/Scripts/System/System_Gravity.cs
+++ Mess Around Game/Assets/Scripts/System/System_Gravity.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class System_Gravity : MonoBehaviour {
 
     public float force = 9.81f; //apply gravity
-    private Vector2 newDir;
+    public Vector2 newDir;
 
     private Quaternion localRotation; // 
     public float smooth = 1.0f; // ajustable speed from Inspector in Unity editor
@@ -28,15 +28,18 @@ public class System_Gravity : MonoBehaviour {
 #if UNITY_EDITOR
 		smoothRot = Quaternion.Slerp (localRotation, Quaternion.AngleAxis (Input.GetAxisRaw("Horizontal") * Mathf.Rad2Deg, Vector3.forward), smooth);
 		//newDir = new Vector2(Input.GetAxisRaw("Horizontal"), -1f);
-        newDir = smoothRot * -Vector2.up;
+        newDir = smoothRot * Vector2.up;
 #elif UNITY_ANDROID
-		smoothRot = Quaternion.Slerp (localRotation, Quaternion.AngleAxis (Input.acceleration.x * Mathf.Rad2Deg, Vector3.forward), smooth * Time.deltaTime);
+		smoothRot = Quaternion.Slerp (localRotation, Quaternion.AngleAxis (Input.acceleration.x * Mathf.Rad2Deg, Vector3.forward), smooth);
 		//newDir = new Vector2(Input.acceleration.x, -1f);
-        newDir = smoothRot * -Vector2.up;
+        newDir = smoothRot * Vector2.up;
 #endif
 
         //Physics2D.gravity = newDir * force;
         //transform.rotation = smoothRot;
+
+        if (newDir.sqrMagnitude > 1)
+            newDir.Normalize();
 
         //transform.Rotate (new Vector3(0, 0, newDir.x));
         target.transform.Rotate(new Vector3(0, 0, newDir.x));
